af4bc7cdcbaec3c2c0dfaeb7e2896565239ce5ae
AlejandroOrdaz <uaslp_oscar@outlook.com>
2016-11-06 13:05:30 -0600

    Se agrega enemigo
    Se agrega un enemigo en la parte superior de la pantalla aleatoriamente

--- Booga/Space.java
+++ Booga/Space.java
@@ -17,5 +17,14 @@ public class Space extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(700, 500, 1); 
+        agrega();
+    }
+    
+        /**
+     * Agrega a los enemigos en la parte superior de la pantalla aleatoriamente
+     */
+    public void agrega()
+    {
+        addObject(new Enemigo(),Greenfoot.getRandomNumber(600),Greenfoot.getRandomNumber(10));
     }
 }
