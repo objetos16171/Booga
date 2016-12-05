4ac04c250ef321548a03e9458373006238721065
AlejandroOrdaz <uaslp_oscar@outlook.com>
2016-12-04 22:06:45 -0600

    Se documento la clase mundo, se cambio imagenes

--- Booga/Space.java
+++ Booga/Space.java
@@ -19,7 +19,7 @@ public class Space extends World
     private SimpleTimer tiempo3 = new SimpleTimer();
     private SimpleTimer tiempo4 = new SimpleTimer();
     private Enemigo ene;
-    private Boton Start, Salir, Help;
+    private Boton Start, Salir, Help, Cre;
     private LinkedList <GreenfootImage> imagenes;
     private int l = 0; 
      private int ban = 0;
@@ -38,12 +38,12 @@ public class Space extends World
         imagenes.add(new GreenfootImage("Start.png")); //1
         imagenes.add(new GreenfootImage("Salir.png"));//2
         imagenes.add(new GreenfootImage("Help.png")); //3            
-        //imagenes.add(new GreenfootImage("Creditos.jpg")); //4
+        imagenes.add(new GreenfootImage("Creditos.png")); //4
         
         Start = new Boton(getImagen(1));
         Salir = new Boton(getImagen(2));
         Help = new Boton(getImagen(3));
-        //Cre = new Boton(getImagen(4));
+        Cre = new Boton(getImagen(4));
        menu();  
     }
     
@@ -54,8 +54,9 @@ public class Space extends World
     public void menu()
     {
         removeObjects(getObjects(null));
-        addObject(Start, 110, 150);
+        addObject(Start, 115, 210);
         addObject(Help, 720, 400);
+        addObject(Cre, 370, 45);
         Greenfoot.setSpeed(40);
         setBackground(getImagen(0));
     }
@@ -68,7 +69,7 @@ public class Space extends World
     {
         removeObjects(getObjects(null));
 
-        GreenfootImage hp = new GreenfootImage("Ayuda.png");
+        GreenfootImage hp = new GreenfootImage("ayuda.jpg");
         setBackground(hp);
         addObject(Salir,740,450);  
     }
@@ -80,7 +81,7 @@ public class Space extends World
     {
         removeObjects(getObjects(null));
 
-        GreenfootImage hp = new GreenfootImage("Creditos.png");
+        GreenfootImage hp = new GreenfootImage("Creditos.jpeg");
         setBackground(hp);
         addObject(Salir,740,450);  
     }
@@ -101,13 +102,13 @@ public class Space extends World
             l = 1;
             nivel1();
         }
-        /*if(Greenfoot.mouseClicked(Cre)){
-            removeObjects(getObjects(null));
-            Creditos();
-        }*/
          if(Greenfoot.mouseClicked(Help)) {
             removeObjects(getObjects(null));
             Help();
+        }
+        if(Greenfoot.mouseClicked(Cre)){
+            removeObjects(getObjects(null));
+            Creditos();
         }
     }
     
@@ -158,7 +159,7 @@ public class Space extends World
     }
     
     /**
-     * 
+     * Método del nivel 3 del juego
      */
     public void nivel3(){
         ban=0;
@@ -177,6 +178,10 @@ public class Space extends World
         addObject(contnivel,68,20);
     }
     
+    /**
+     * Metodo que agrega a los enemigos, monedas, escudos, vidas,
+     * meteo
+     */
     public void agrega()//se
     {
        if(ban==0){ 
@@ -203,7 +208,9 @@ public class Space extends World
        }  
     }
 
-       
+    /**
+     * Metodo que agrega a los jefe dependiendo del nivel.
+     */
     public void jefe(int jefe)
     {
         ban=1;
@@ -243,6 +250,9 @@ public class Space extends World
           }      
     }
 
+    /**
+     * Metodo que inicializa los tiempos.
+     */
      public void started()
     {
         tiempo.mark();
@@ -257,55 +267,81 @@ public class Space extends World
         }
     } 
     
+    /**
+     * Metodo que regresa las imagenes en una lista.
+     */
     public GreenfootImage getImagen(int n)
     {
         return imagenes.get(n);
     }
     
+    /**
+     * Metodos para regesar los botones
+     */
     public Boton getJugar()
     {
         return Start;
     }
-    
     public Boton getAyuda()
     {
         return Help;
     }
     
+    /**
+     * Metodo que agrega el escudo al jugador.
+     */
     public void generaEscudo(int Bx, int By ) //Genera el escudo burbuja 
     {
         addObject(new EscudoBurbuja(),Bx,By);
     }
     
     public void remueveEscudo() //Remueve el escudo
-    { 
-        
+    {     
     }
-    
-    public int getNumBalls() { return numBalas; }  //regresa el numero de balas
-    
+   
+    /**
+     * Metodo que regresa la cantidad de balas que tiene el jugador.
+     */
+   public int getNumBalls() { return numBalas; }  //regresa el numero de balas
+   
+   /**
+    * Metodo que agrega una bala para el jugadoor.
+    */
     public void newBalas(int Bx, int By)  //anade balas al esenario
    {
       numBalas--;
       if(numBalas>0){        // condicion capacidad de balas
         addObject(new Bala(), Bx, By);
       }
-    }
-    
-   public void setBalas(int val){
-       numBalas=val;
    }
-
-   public void newBalljefe(int Ax, int Ay){
+    
+  /**
+   * Metodo que se iguala la cantidad de balas que tiene disponoble el jugador.
+   */
+  public void setBalas(int val){
+       numBalas=val;
+  }
+   
+  /**
+  * Metodo que hañande una bala del jefe.
+  */
+   public void newBalljefe(int Ax, int Ay)
+   {
      addObject(new Balljefe(), Ax, Ay);
    }
     
-  
+  /**
+   * Metodo que se encarga de quitar una vida al jugador.
+   */
    public void muerte()
    {
        decrementaVidas();
    }
    
+   /**
+    * Metodo que se encarga de poner la imagen de ganar cuando haya 
+    * terminado el juego.
+    */
    public void Gana(){
        removeObjects(getObjects(null));
        GreenfootImage gan = new GreenfootImage("Ganar.jpg");
@@ -324,6 +360,10 @@ public class Space extends World
            muere();
        }
     }
+    
+    /**
+     * Metodo que loca la imagen de perder cuando ya no tiene vidas
+     */
     public void muere()
     {
         removeObjects(getObjects(null));
@@ -339,16 +379,26 @@ public class Space extends World
     {
         contPunt.add(+1);
     }
+    
+    /**
+     * Método que se encarga de sumar una vida al jugador
+     */
     public void incrmentaVida() 
     {
         contVidas.add(+1);
     }
     
+    /**
+     * Método que se encarga de sumar a los enemigos
+     */
     public void cuentaEnemigos()
     {
         contMuer++;
     }
     
+    /**
+     * Metodo que regresa la cantidad de muertes de los enemigos
+     */
     public int getcuE()
     {
         return contMuer;
